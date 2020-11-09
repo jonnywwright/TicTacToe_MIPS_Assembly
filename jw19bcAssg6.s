39a21b033c4b97c9d2aa583b8e4da1f588db916f
Jon Wright <wright.jonny@icloud.com>
2020-11-09 02:25:39 -0500

    Update jw19bcAssg6.s
    Implemented the GETVALFROMPOSITION submodule.

--- jw19bcAssg6.s
+++ jw19bcAssg6.s
@@ -35,14 +35,25 @@ addiu   $sp,$sp,4
 
 addiu   $sp,$sp,-4
 sw      $ra,0($sp)
-jal     GETINPUT
+jal     GETINPUT ### Saving input to $s0
 lw      $ra,0($sp)
 addiu   $sp,$sp,4
 
 li  $v0,1                  # Print the input from the print input function
 move  $a0,$s0
+syscall
 
+move $s3,$s0
 
+addiu   $sp,$sp,-4
+sw      $ra,0($sp)
+jal     GETVALFROMPOSITION ### Passing position in $s3
+lw      $ra,0($sp)
+addiu   $sp,$sp,4
+
+li  $v0,1                  # Print the input from the print input function
+move  $a0,$s3
+syscall
 ################ This block is just here for testing
 
  #li      $v0,4               #Ready the printer    
@@ -50,7 +61,7 @@ move  $a0,$s0
 #syscall                     #Print the welcome message
 jr		$ra					# Exit game
 
-
+#### Input saved in $s0
 GETINPUT:
 addiu   $sp,$sp,-4
 sw      $ra,0($sp)
@@ -130,7 +141,26 @@ COLISWON:
 ### CHECKS IF DIAGNOL AT POSITION IS WON
 DIAGNOLISWON:
 
+### Takes the position 1 - 9, uses modulo and division 
+### to get the row and col
+### Assumes position is stored in $s3
 GETVALFROMPOSITION:
+la $t0,BOARD
+addi $s3, -1    #decrement position by one because zero indexing is awesome
+li   $a0, 3
+div  $s3,$a0     #modulo is in Hi quot in Lo
+mfhi $a1        #modulo now in $a1
+mflo $a2        #quot now in $a2
+mult $a0,$a2    #zero indexed row now stored in $lo
+mflo $a0        # move back into $a0
+add  $a0,$a0, $a1   #the column offset now added to $a0
+li $a1, 4       # byte offset size
+mult $a0, $a1   #mult by the byte offset size
+mflo $a0        #move total offset into $a0
+add  $t0,$t0,$a0 #t0 now indexed at the correct POSITION
+lw  $s3, 0($t0)  # return the value back via $s3
+jr  $ra
+
 
 SETVALFROMPOSITION:
 
